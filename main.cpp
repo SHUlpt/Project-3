e6380755bf76d060b38c8774442e65eb421dbc76
SHUlpt <1424526265@qq.com>
2020-06-07 16:03:11 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//#define ONLINE_JUDGE
+#include <bits/stdc++.h>
+using namespace std;
+#define mem(a,b) memset(a,b,sizeof(a))
+#define endl '\n'
+typedef long long ll;
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("D:\\SHUlpt\\ACM\\Code\\in.txt", "r", stdin);
+    // freopen("D:\\SHUlpt\\ACM\\Code\\out.txt", "w", stdout);
+#endif
+	ios::sync_with_stdio(0),cin.tie(0);
+	cout << "hello world" << endl;
+}
